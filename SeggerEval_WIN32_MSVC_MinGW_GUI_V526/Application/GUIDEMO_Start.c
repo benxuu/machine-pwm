852939e3e6a2f7a2e53bb0869a2a4e5a21f58ea0
ben <benxuu@163.com>
2019-03-28 11:30:35 +0800

    2019year03month28day

---
+++ SeggerEval_WIN32_MSVC_MinGW_GUI_V526/Application/GUIDEMO_Start.c
@@ -0,0 +1,43 @@
+/*********************************************************************
+*                SEGGER Microcontroller GmbH & Co. KG                *
+*        Solutions for real time microcontroller applications        *
+**********************************************************************
+*                                                                    *
+*        (c) 1996 - 2014  SEGGER Microcontroller GmbH & Co. KG       *
+*                                                                    *
+*        Internet: www.segger.com    Support:  support@segger.com    *
+*                                                                    *
+**********************************************************************
+
+** emWin V5.26 - Graphical user interface for embedded applications **
+emWin is protected by international copyright laws.   Knowledge of the
+source code may not be used to write a similar product.  This file may
+only be used in accordance with a license and should not be re-
+distributed in any way. We appreciate your understanding and fairness.
+----------------------------------------------------------------------
+File        : GUIDEMO_Start.c
+Purpose     : GUIDEMO initialization
+----------------------------------------------------------------------
+*/
+
+#include "GUIDEMO.h"
+
+/*********************************************************************
+*
+*       MainTask
+*/
+void MainTask(void) {
+  #if GUI_WINSUPPORT
+    WM_SetCreateFlags(WM_CF_MEMDEV);
+  #endif
+  GUI_Init();
+  #if GUI_WINSUPPORT
+    WM_MULTIBUF_Enable(1);
+  #endif
+  //GUIDEMO_Main();
+    CreatePWMControl(WM_HBKWIN); //创建窗体,父窗体是桌面背景
+    while(1) {GUI_Delay(20);} //调用GUI_Delay函数延时20MS(最终目的是调用GUI_Exec()函数)
+}
+
+/*************************** End of file ****************************/
+
